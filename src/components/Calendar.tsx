708a0120f179e7e50df0482012232b499b56aa3d
doyou04 <kimsul387@gmail.com>
2023-07-18 18:30:47 +0900

    작업

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -2,14 +2,22 @@ import React from 'react';
 import '../style/Calendar.scss';
 
 const Calendar = () => {
+  const newDate = new Date();
+  const currentYear = new Date(newDate).getFullYear();
+  const currentMonth = new Date(newDate).getMonth() + 1;
+  const firstDay = new Date(newDate.setDate(1))
   const weekTitles = ['SUN','MON','TUE','WED','THU','FRI','SAT']
   const listItems = weekTitles.map((weekTitle:string, i:number) => 
     <li key={i}>{weekTitle}</li>
   )
 
+ 
+
   return (
     <div className="calendar_main">
       <header>
+        <h1>2023</h1>
+        <h2>7월</h2>
         <ul>{listItems}</ul>
       </header>
     </div>
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -3,12 +3,10 @@ import '../style/Main.scss';
 import Calendar from './Calendar';
 
 const Main = () => {
+
+
   return (
     <div className="main">
-      <header className="main_header">
-        <h1>2023</h1>
-        <h2>7월</h2>
-      </header>
       <Calendar />
     </div>
   );
