b74ee6ace1b1431078bb61cc5d0b2635b763e8eb
doyou04 <kimsul387@gmail.com>
2023-07-24 15:46:30 +0900

    푸쉬

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -15,26 +15,56 @@ const Calendar = () => {
   const [selectedYear, setSelectedYear] = useState<number>(currentYear);
   const [selectedMonth, setSelectedMonth] = useState<number>(currentMonth);
   const [selectedDay, setSelectedDay] = useState<number>(currentDay);
-  // 현재 날짜 Mon Jul 24 2023 00:00:00 GMT+0900
-  const currentLastDate:Date = new Date(selectedYear, selectedMonth-1, selectedDay)
+  // 현재달 첫째날 요일 0~6 (일~토)
+  const day:number = new Date(selectedYear, selectedMonth-1, 1).getDay()
+  // 현재 달 마지막 날짜(일)
+  const currentLastDate:number = new Date(selectedYear, selectedMonth, 0).getDate()
   // 현재 달 첫날 Mon Jul 24 2023 00:00:00 GMT+0900
   const currentfirstDay:Date = new Date(newDate.setDate(1))
   // 지난 달 마지막날 Mon Jul 24 2023 00:00:00 GMT+0900
   const pastFinalDay:Date = new Date(newDate.setDate(0))
 
-  console.log(selectedDay)
+  const returnDay = useCallback(() => {
+    let dayArr = []
+
+    for(const nowDay of weekTitles){
+      if(weekTitles[day] === nowDay){
+        for(let i=0; i<currentLastDate; i++){
+          dayArr.push(
+            <div key={i+1}>{i+1}</div>
+          )
+        }
+      }else{
+        dayArr.push(
+          <div className="box_date"></div>
+        )
+      }
+
+    }
+    return dayArr;
+  },[selectedYear, selectedMonth, currentLastDate])
+ 
+  const ActionYearLeft = () => {
+    setSelectedYear(selectedYear-1)
+  }
+
+  console.log(day)
   //  console.log(firstDay)
 
   return (
     <div className="calendar_main">
       <header>
-        {/* <h1>{currentYear}</h1>
+        <h1>
+          <span className="arrow_left" onClick={ActionYearLeft}>❮</span>
+          {selectedYear} 
+          <span className="arrow_right">❯</span>
+        </h1>
         <h2>{currentMonth}월</h2>
-         */}
+        
       </header>
-      {/* <ul>{listItems}</ul> */}
+      <ul>{listItems}</ul>
       <div className="calendar_cnt">
-
+         {returnDay()}
       </div>
     </div>
   );
