45ca9b695a79ff39513bc53729a6776d0fb8e802
doyou04 <kimsul387@gmail.com>
2023-07-25 18:54:06 +0900

    스타일 작업

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -1,4 +1,4 @@
-import React, {useCallback, useState} from 'react';
+import React, {useCallback, useEffect,  useState} from 'react';
 import '../style/Calendar.scss';
 import classnames from 'classnames';
 
@@ -8,72 +8,89 @@ const Calendar = () => {
   const currentMonth:number = new Date().getMonth() + 1;
   const currentDay:number = new Date().getDate()
   // 요일
-  const weekTitles:string[]= ['SUN','MON','TUE','WED','THU','FRI','SAT']
+  const weekTitles:string[]= ['Sun','Mon','Tue','Wed','Thu','Fri','Sat']
   const listItems = weekTitles.map((weekTitle:string, i:number) => 
-    <li key={i} className={classnames({sunday: i === 0, saturday: i === 6})}>{weekTitle}</li>
+    <li key={`weekTitles${i}`} className={classnames({sunday: i === 0, saturday: i === 6})}>{weekTitle}</li>
   )
   //선택 연도, 달, 일
   const [selectedYear, setSelectedYear] = useState<number>(currentYear);
   const [selectedMonth, setSelectedMonth] = useState<number>(currentMonth);
-  const [selectedDay, setSelectedDay] = useState<number>(currentDay);
-  const [active, setActive ] = useState<boolean>();
-  // 현재달 첫째날 요일 0~6 (일~토)
-  const day:number = new Date(selectedYear, selectedMonth-1, 1).getDay()
-  // 현재 달 마지막 날짜(일)
-  const currentLastDate:number = new Date(selectedYear, selectedMonth, 0).getDate()
+  const [selectedDay, setSelectedDay] = useState<number>(0);
+  // 선택 달 마지막 날짜(일)
+  const selectLastDate:number = new Date(selectedYear, selectedMonth, 0).getDate()
   // 현재 달 첫날 Mon Jul 24 2023 00:00:00 GMT+0900
   const currentfirstDay:Date = new Date(newDate.setDate(1))
   // 지난 달 마지막날 Mon Jul 24 2023 00:00:00 GMT+0900
   const pastFinalDay:Date = new Date(newDate.setDate(0))
-  // 년 이전 버튼
-  const ActionYearLeft = ():void => {
-    setSelectedYear(selectedYear - 1)
+  // 달 이전 버튼
+  const ActionMonthLeft = useCallback(() => {
+    if(selectedMonth === 1){
+      setSelectedMonth(12)
+      setSelectedYear(selectedYear - 1)
+    }else{
+      setSelectedMonth(selectedMonth - 1)
+    }
+    setSelectedDay(0)
+  },[selectedMonth])
+  // 달 다음 버튼
+  const ActionMonthRight =  useCallback(() => {
+    if(selectedMonth === 12){
+      setSelectedYear(selectedYear + 1)
+      setSelectedMonth(1)
+    }else{
+      setSelectedMonth(selectedMonth + 1)
+    }
+     setSelectedDay(0)
+  },[selectedMonth])
+  // 선택한 날짜
+  const selectActive = (i:number):void=>  {
+    setSelectedDay(i)
   }
-  // 년 다음 버튼
-  const ActionYearRight = ():void => {
-    setSelectedYear(selectedYear + 1)
-  }
- 
+  // 선택 달 cell
   const returnDay = useCallback(() => {
-    let dayArr = []
+    // 선택 달 첫째날 요일 0~6 (일~토)
+    let dayArr:number[] = [];
 
     for(const nowDay of weekTitles){
+      const day:number = new Date(selectedYear, selectedMonth-1, 1).getDay()
       if(weekTitles[day] === nowDay){
-        for(let i=0; i<currentLastDate; i++){
-          dayArr.push(
-            <div key={i+1}
-            className={classnames({today: currentYear === selectedYear && currentMonth === selectedMonth && currentDay === i + 1},{select:currentYear === selectedYear && currentMonth === selectedMonth && selectedDay === i+1 ? true: false})}
-            onClick={() => selectActive(i)}>{i+1}</div>
-          )
+        for(let i=0; i<selectLastDate; i++){
+          dayArr.push(i+1)
         }
       }else{
-        dayArr.push(
-          <div className="box_date"></div>
-        )
+        dayArr.push(0)
       }
     }
     return dayArr;
-  },[selectedYear, selectedMonth, currentLastDate])
+  },[selectLastDate,selectedYear,selectedMonth])
  
-  const selectActive = (i:number):void => {
-    // setActive([])
-    setSelectedDay(i+1)
-  }
-
-
+ 
   return (
     <div className="calendar_main">
       <header>
-        <h1>
-          <span className="arrow_left" onClick={ActionYearLeft}>❮</span>
-          {selectedYear} 
-          <span className="arrow_right" onClick={ActionYearRight}>❯</span>
-        </h1>
-        <h2>{selectedMonth}월</h2>
+        <h1>{selectedYear}</h1>
+        <h2>
+          <span className="arrow_left" onClick={ActionMonthLeft}>❮</span>
+          {selectedMonth}월
+          <span className="arrow_right" onClick={ActionMonthRight}>❯</span>
+        </h2>  
       </header>
       <ul>{listItems}</ul>
       <div className="calendar_cnt">
-         {returnDay()}
+         {
+          returnDay().map((value:number, index:number) => {
+            return(
+              value > 0 ?
+              <div key={`id_${value}`}
+              className={classnames(
+                {today: currentYear === selectedYear && currentMonth === selectedMonth && currentDay === value},
+                {selectActive: value === selectedDay }
+              )}
+              onClick={() => selectActive(value)}>{value}</div>:<div className="box_date"></div>
+            )
+          })
+          
+         }
       </div>
     </div>
   );
